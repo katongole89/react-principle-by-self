d06a0fe4c46ecff15ad1132db3584a8b0cd9cfe9
katongole89 <katongolerichard089@gmail.com>
2022-08-03 16:04:51 +0300

    improved filtering on search

--- src/pages/SearchableDropDown/index.jsx
+++ src/pages/SearchableDropDown/index.jsx
@@ -23,7 +23,8 @@ const nameList = [
 ]
 
 const defaultState ={
-    searchInput:''
+    searchInput:'',
+    nameList: []
 }
 
 
@@ -31,28 +32,40 @@ export const reducer = (state, action) => {
   if (action.type === 'UPDATE_SEARCH') {
     return { ...state, searchInput: action.payload };
   }
+  if (action.type === 'UPDATE_NAME_LIST') {
+    return { ...state, nameList: action.payload };
+  }
 
   throw new Error('no matching action type');
 };
 
 
-
-
-
 const SearchableDropdown = () =>{
     const [state, dispatch] = useReducer(reducer, defaultState)
+    const filterData = useCallback((searchValue)=>{
+        const newData = nameList.filter((option)=>{
+            return option.name.includes(searchValue)
+        })
+        return newData
+    }, [])
+
     const handleChange = useCallback((e)=>{
-        dispatch({type:'UPDATE_SEARCH', payload: e.target.value})
-    })
+        // dispatch({type:'UPDATE_SEARCH', payload: e.target.value})
+        const newData = filterData(e.target.value)
+        console.log(newData)
+        dispatch({type:'UPDATE_NAME_LIST', payload: newData})
+        
+    }, [])
 
-    console.log(state)
+    React.useEffect(()=>{
+        dispatch({type:'UPDATE_NAME_LIST', payload: nameList})
+    }, [])
 
-
-
+    
     return(
         <div style={{display:'flex', width:'100%', minHeight:'90vh', alignItems:'center', justifyContent:'center', flexDirection:'column'}}>
             <TheInput onChange={handleChange}/>
-            <ConditionalDisplay nameList={nameList}/>
+            <ConditionalDisplay nameList={state.nameList}/>
         </div>
         
     )
