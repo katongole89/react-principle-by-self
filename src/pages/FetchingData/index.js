5a7a8732dd664835fda1f3f1d1820465d3528d3e
katongole89 <katongolerichard089@gmail.com>
2022-09-26 16:44:41 +0300

    created setup for fetching data using fetch -- component

--- src/pages/FetchingData/index.js
+++ src/pages/FetchingData/index.js
@@ -2,14 +2,39 @@ import React from 'react'
 
 //using axios to get and to post
 //using fetch to get and to post
+const UsingFetchSetup = () =>{
+    return(
+        <div style={{display:'flex',width:'100%', border: '1px solid gray', minHeight:'200px'}}>
+            <div style={{flex:1}}>
+                <button>Get data</button>
+            </div>
+            <div style={{flex:1}}>
+                Post data side
+            </div>
+        </div>
+    )
+}
+
+
 
 const FetchingData = () =>{
     return(
-        <div>
-            FetchingData
-        </div>
-    )
+        <>
+            <div>
+                FetchingData
+            </div>
+            <div style={{display:'flex', width:'100%'}}>
+                <div style={{flex:1,margin:'10px'}}>
+                    <UsingFetchSetup/>
+                </div>
+                <div style={{flex:1}}>
+                    second part
+                </div>
 
+
+            </div>
+        </>
+    )
 }
 
 export default FetchingData
