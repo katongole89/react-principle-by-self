b344cf15cf664e2e4a01cc1c56d42f0c7c88328c
katongole89 <katongolerichard089@gmail.com>
2022-09-26 17:10:54 +0300

    added loading setup for fetch

--- src/pages/FetchingData/index.js
+++ src/pages/FetchingData/index.js
@@ -2,14 +2,35 @@ import React, {useState, useEffect} from 'react'
 
 //using axios to get and to post
 //using fetch to get and to post
-const UsingFetchSetup = () =>{
+const UsingFetchSetup = ({fetchData, isFetching}) =>{
     return(
         <>
             <span style={{fontWeight:'bold'}}>Fetch</span>
             <div style={{display:'flex',width:'100%', border: '1px solid gray', minHeight:'200px'}}>
                 <div style={{flex:1,display:'flex',flexDirection:'column'}}>
                     <span>Get data</span>
-                    <button>Get data</button>
+                    <div>
+                        <button>Get data</button>
+                    </div>
+                    {isFetching?
+                        <div>Fetching data..</div>
+                        :
+                        <div>
+                            {fetchData.length < 1?
+                                <span>No data to display</span>
+                            :
+                                (fetchData.map(()=>{
+                                    return(
+                                        <div>
+                                            single component
+                                        </div>
+                                    )
+                                }))
+                            }
+                        </div>
+                    }
+                    
+                    
                 </div>
                 <div style={{flex:1}}>
                     Post data side
@@ -53,7 +74,7 @@ const FetchingData = () =>{
             </div>
             <div style={{display:'flex', width:'100%'}}>
                 <div style={{flex:1,margin:'10px'}}>
-                    <UsingFetchSetup/>
+                    <UsingFetchSetup fetchData={state.fetchData} isFetching={state.isFetching}/>
                 </div>
                 <div style={{flex:1,margin:'10px'}}>
                     <UsingAxiosSetup/>
