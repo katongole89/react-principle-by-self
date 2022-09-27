0bffc16022596012ffda3d13026503ea1df772e7
katongole89 <katongolerichard089@gmail.com>
2022-09-27 13:28:39 +0300

    added state and created handleInputChange function

--- src/pages/Forms/index.js
+++ src/pages/Forms/index.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const FormsComponent = () => {
+    const [state, setState] = useState({
+        username: '',
+        password:''
+    })
+
+    const handleChangeFields = (e) => {
+        setState({...state, [e.target.name]: e.target.value})
+    }
+
+    console.log(state)
+
+
+
+
+
     return(
         <div style={{display:'flex', justifyContent:'center', alignItems:'center', width:'100%', minHeight:'100vh'}}>
             <form>
                 <div style={{display:'flex', flexDirection:'column'}}>
                     <div>
                         <span>Username</span>
-                        <input name='username'/>
+                        <input name='username' onChange={handleChangeFields}/>
                     </div>
                     <div>
                         <span>password</span>
-                        <input name='password'/>
+                        <input name='password' onChange={handleChangeFields}/>
                     </div>
                     <div>
                         <button type='submit'>Submit the data</button>
