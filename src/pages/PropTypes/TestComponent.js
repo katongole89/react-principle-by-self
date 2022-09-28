2793846fe4d73478ac154d18c26dd0cb5ea184ed
katongole89 <katongolerichard089@gmail.com>
2022-09-28 16:35:41 +0300

    added the test component for prop types

---
+++ src/pages/PropTypes/TestComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const TestComponent = () => {
+    return(
+        <div>
+            Test component
+        </div>
+    )
+
+}
+
+export default TestComponent
--- src/pages/PropTypes/index.js
+++ src/pages/PropTypes/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import TestComponent from './TestComponent'
 
 const PropTypesComponent = () => {
     return(
         <div>
             Prop types
+            <TestComponent/>
         </div>
     )
 }
