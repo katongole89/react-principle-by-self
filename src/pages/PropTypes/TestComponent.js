422750e86ae67a963744ce5ece60b23cdbff4f1a
katongole89 <katongolerichard089@gmail.com>
2022-09-28 16:43:48 +0300

    completed props setup

--- src/pages/PropTypes/TestComponent.js
+++ src/pages/PropTypes/TestComponent.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const TestComponent = () => {
+const TestComponent = (image, name, price) => {
     return(
         <div>
             Test component
         </div>
     )
 
+}
+
+TestComponent.propTypes = {
+    image: PropTypes.object.isRequired,
+    name:PropTypes.string.isRequired,
+    price:PropTypes.number.isRequired
 }
 
 export default TestComponent
