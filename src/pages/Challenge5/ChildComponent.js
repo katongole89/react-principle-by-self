5ae7923178df72b1f7eac6019c170d341b4b755b
katongole89 <katongolerichard089@gmail.com>
2022-09-30 15:25:11 +0300

    added the challenge 5 question

--- src/pages/Challenge5/ChildComponent.js
+++ src/pages/Challenge5/ChildComponent.js
@@ -1,3 +1,16 @@
+// Create a functional component that renders an object of users extracted from context using React’s useContext() hook.
+// The object’s keys are the users' names and the associated values are booleans that indicate whether the users are online or offline.
+// The object should be defined in the parent component’s state and the object, as well as its associated setState() function,
+// should be provided to the aforementioned functional component via context.
+// Here is an example object initialized in state:
+
+// Again, userState and setUserState() should be passed to the child of their enclosing component via context.
+// The child should extract userState and setUserState() using the useContext() hook.
+// It should display each user’s name alongside an icon indicating if the user is online (🟢) or offline (🔴).
+// Every two seconds, the component should change the status of a random user to the opposite of their current status.
+
+
+
 import React, {useContext, useEffect} from 'react'
 import { GeneralContext } from '.'
 import offline from '../../assets/images/ofline.png'
