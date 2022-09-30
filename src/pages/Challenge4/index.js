64f45207eb4dda26d3f5e647b00c35652a7ebb93
katongole89 <katongolerichard089@gmail.com>
2022-09-30 10:23:04 +0300

    added the challenge 4 qtn

--- src/pages/Challenge4/index.js
+++ src/pages/Challenge4/index.js
@@ -1,3 +1,18 @@
+// Write a component that renders five stacked identical images of ladder pieces.
+// When the user hovers over a piece of the ladder, that ladder piece (as well as all the pieces below it)
+// should expand to a larger size. The starting position of the component (when the user’s mouse is not over any of the images),
+// should look like this. The component should return to this state when the user is not hovering over any images.
+// The image can be found at “https://raw.githubusercontent.com/jusshe/coding-challenge-pictures/main/ladder.png”.
+
+// Below are two other possible states — when the user is hovering their mouse over the second image from the bottom and when the user
+// is hovering their mouse over the fourth image from the bottom, respectively. Importantly,
+// your solution should also be easily generalizable to any number of ladder segments.
+
+// Though the solution section will only create a component with only the features discussed in the specification up to this point,
+// you can also try making the ladder grow on its own from the image on the bottom to the one at the top using a timer and useEffect().
+
+
+
 import React, {useState, useEffect} from 'react'
 import ladderImage from '../../assets/images/ladder.png'
 
