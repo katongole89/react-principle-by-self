91b487acf6e89747009c734543b0c0b96976622d
katongole89 <katongolerichard089@gmail.com>
2022-09-30 15:14:04 +0300

    completed challenge 5

--- src/pages/Challenge5/ChildComponent.js
+++ src/pages/Challenge5/ChildComponent.js
@@ -1,11 +1,44 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 import { GeneralContext } from '.'
 
 const ChildComponent = () =>{
-    const contextData = useContext(GeneralContext)
+    const {state, setState} = useContext(GeneralContext)
+
+    useEffect(()=>{
+        console.log('setting interval')
+        const randomPick = () => {
+            let arrObj = Object.entries(state.users)
+            let keyArray = Object.keys(arrObj)
+            let randomIndex = Math.floor(Math.random()*keyArray.length)
+            setState({...state, users:{...state.users, [arrObj[randomIndex][0]]: !state.users[arrObj[randomIndex][0]] } })
+        }
+
+        const newInterval = setInterval(randomPick, 2000)
+        return ()=> clearInterval(newInterval)
+
+    }, [])
+
 
     return(
-        <div>Child component</div>
+        <div style={{marginTop:'30px'}} >
+            {Object.entries(state.users).map(([name, bool])=>{
+                return(
+                    <div key={name} style={{display:'flex', width:'100%', justifyContent:'center', alignItems:'center'}}>
+                        <div>
+                            <span>{name}: </span>
+                        </div>
+                        <div>
+                            {bool?
+                                <span>Green</span>
+                                :
+                                <span>Red</span>
+                            }
+                        </div>
+                    </div>
+                )
+            })}
+
+        </div>
     )
 }
 
