c8352d2a20dc38becdf96d4520e80af557204e38
katongole89 <katongolerichard089@gmail.com>
2022-09-30 16:23:35 +0300

    added the challenge6 qtn

--- src/pages/Challenge6/index.js
+++ src/pages/Challenge6/index.js
@@ -1,3 +1,35 @@
+// Create a functional component to render a list of tasks (along with their subtasks) from an object.
+// The subtasks should be organized into two columns, completed and to be completed, based on the boolean value associated with each task.
+// The object will have the following format: any number of keys where each key is the text of the task,
+// an array of any number of objects associated with each key, and finally, an object with one field (the subtask text) 
+// and one associated boolean value at every index of the array.
+// Here is an example of one such object:
+
+// const INITIAL_LIST = {
+//     "Organize closet": [
+//       { "Donate old clothes and shoes": false },
+//       { "Buy new shelf": false },
+//       { "Put in shelf by color": false },
+//     ],
+//     "Finish homework": [
+//       { "Finish math homework": false },
+//       { "Finish science homework": false },
+//       { "Finish Reactjs homework": false },
+//     ],
+//     "Achieve nirvana": [
+//       { "Meditate a little": false },
+//       { "Gain some wisdom": false },
+//     ],
+//   };
+
+
+// When the user clicks on an uncompleted subtask, 
+// it should move from the uncompleted list to the completed list.
+// Likewise, when the user clicks on a completed subtask,
+// it should move from the completed list to the uncompleted list
+
+
+
 import React, {useEffect, useState} from 'react'
 
 const SubTasks = ({subTasks}) => {
