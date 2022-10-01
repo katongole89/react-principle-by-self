02e1ae96175992e47fe58168c34ef10853023cb5
katongole89 <katongolerichard089@gmail.com>
2022-10-01 10:58:39 +0300

    created the basic component with state and rendering the entered list for challenge 7

--- src/pages/Challenge7/index.js
+++ src/pages/Challenge7/index.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Challenge7 = () =>{
+const AddedString = ({str}) =>{
     return(
         <div>
-            Challenge7
+            <span>{str}</span>
+        </div>
+        
+    )
+}
+
+const Challenge7 = () =>{
+    const [state, setState] = useState({
+        listWords:[],
+        word:''
+    })
+
+    const handleChangeInput = (e) => {
+        setState({...state, word:e.target.value})
+    }
+
+    const handleSubmitWord = (e) =>{
+        e.preventDefault()
+        setState({...state, word:'', listWords:[state.word, ...state.listWords]})
+    }
+
+
+    return(
+        <div style={{display:'flex', width:'100%', justifyContent:'center', alignItems:'center', flexDirection:'column', minHeight:'100vh'}}>
+            <div>
+                <form onSubmit={handleSubmitWord}>
+                    <input value={state.word} onChange={handleChangeInput} />
+                </form>
+            </div>
+            <div style={{marginTop:'10px'}}>
+                {state.listWords.map((word)=>{
+                    return(
+                        <AddedString str={word} />
+                    )
+                })}
+
+            </div>
         </div>
     )
 }
