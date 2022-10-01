1f7beed0921b1a2fbccd987a3837ef29dbebd543
katongole89 <katongolerichard089@gmail.com>
2022-10-01 11:48:59 +0300

    added the simple modal logic

--- src/pages/SimpleModal/index.js
+++ src/pages/SimpleModal/index.js
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 
 const SimpleModal = () =>{
+    const modalRef = useRef()
+    const [state, setState] = useState({
+        modalOpen:false,
+        modalContent:''
+    })
+
+    const handleClick = () => {
+        setState({...state, modalOpen:true})
+    }
+
+    useEffect(()=>{
+        const closeModal = () =>{
+            setState({...state, modalOpen:false})
+        }
+ 
+        document.addEventListener('mousedown', closeModal)
+
+        return () => document.removeEventListener('mousedown', closeModal)
+
+    }, [state.modalOpen])
+
     return(
         <>
-        <div style={{display:'flex', width:'100%', height:'100vh'}}>
+        <div style={{display:'flex', width:'100%', height:'100vh', position:'relative', justifyContent:'center', alignItems:'center'}}>
+            <button onClick={handleClick}>click me</button>
 
         </div>
-        <div style={{height:'100px', width:'100px', inset:0, backgroundColor:'black' }}>
+        {state.modalOpen?
+            <div style={{height:'100px', width:'100px', inset:0, backgroundColor:'black', position:'absolute' }} ref={modalRef} >
 
-        </div>
+            </div>
+            :
+            null
+        }
+        
         </>
     )
 }
